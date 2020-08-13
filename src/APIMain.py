e7558d852d1016151bed11981ef7237f220a2300
szken3 <dddlia3@gmail.com>
2020-08-13 22:29:42 +0900

    商品検索APIの動作確認

--- src/APIMain.py
+++ src/APIMain.py
@@ -13,49 +13,32 @@ sys.path.append(cdir)
 
 import apiInfo
 
-url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628"
+# 商品検索APIのリクエストURL
+# 後ろの日付はバージョンの代わり
+url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?"
 
 # これもこんなにめんどくさかったか？
 api_id = apiInfo.RAKUTEN_APPLICATION_ID
-category_num = '564500'
-format_num = '2'
-page_num = '1'
 
-result = {}
-for page_int in range(3):
-    page_num = str(page_int+1)
-    response = requests.get(url, params = {
-    'applicationId':api_id,
-    'genreId':category_num,
-    'formatVersion':format_num,
-    'page':page_num}
-    )
-    json_data = response.json()
+# バドミントンをキーワードに価格降順をjson形式
+# パラメータに関しては公式を参照 https://webservice.rakuten.co.jp/api/ichibaitemsearch/
+keyword = "バドミントン"
+sort = "-itemPrice"
+format = "json"
 
-    for rank in range(30):
-        try:
-            rank_num = json_data['Items'][rank]['rank']
-            ItemName_str = json_data['Items'][rank]['itemName']
-            ItemUrl_str = json_data['Items'][rank]['itemUrl']
-            ShopName_str = json_data['Items'][rank]['shopName']
-            ItemPrice_str = json_data['Items'][rank]['itemPrice']
-            PointRate_str = json_data['Items'][rank]['pointRate']
-            add_data = {}
-            add_data['ItemName'] = ItemName_str
-            add_data['StoreName'] = ShopName_str
-            add_data['ItemUrl'] = ItemUrl_str
-            add_data['ItemPrice'] = ItemPrice_str
-            add_data['PointRate'] = PointRate_str
-            result[rank_num] = add_data
-        except IndexError:
-            break
-        except KeyError:
-            break
+response = requests.get(url, params = {
+     'applicationId':api_id,
+     'keyword':keyword,
+     'sort':sort,
+     'format':json}
+     )
 
-#pprint.pprint(result)
-#pprint.pprint(response.json())
-#with open('download_r.json', 'w') as f:
-    #json.dump(json_data, f, ensure_ascii=False, indent=4)
+# 一回だけ抜いただけなので1ページに表示される上限30アイテムの情報のみ出力される
+json_data = response.json()
+for i in json_data["Items"]:
+    item = i["Item"]
+    print(item["itemName"] + item["itemUrl"])
 
-res = pd.DataFrame(result).T
-res.to_csv('to_csv_out.csv', encoding='utf_8_sig')
+# ファイル出力
+# json_file = open('test_json.json', 'w')
+# json.dump(json_data, json_file)
